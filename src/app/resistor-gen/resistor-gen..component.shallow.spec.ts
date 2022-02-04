d1a12b505ac696f070f9aa68d475f4bed5bcb07d
Joseph <lai225joseph@hotmail.com>
2022-02-04 17:20:47 +0800

    change colorBand type from number to enum

--- src/app/resistor-gen/resistor-gen..component.shallow.spec.ts
+++ src/app/resistor-gen/resistor-gen..component.shallow.spec.ts
@@ -2,6 +2,7 @@
 import { TestBed, ComponentFixture } from "@angular/core/testing";
 import { ResistorGenComponent } from "./resistor-gen.component";
 import { By } from "@angular/platform-browser";
+import {ResistorColorBand} from "./resistor-gen.component";
 
 describe('ResistorGenComponent (shallow tests)', () => {
   let fixture: ComponentFixture<ResistorGenComponent>;
@@ -20,19 +21,19 @@ describe('ResistorGenComponent (shallow tests)', () => {
   it('the backgroundColor of the first band of the resistor shall be yellow if select yellow ', () => {
     fixture.detectChanges();
     fixture.componentInstance.color = yellowColor;
-    fixture.componentInstance.setColorBand(0);
-    let bandColor = fixture.debugElement.queryAll(By.css(theBandClassName))[0];
+    fixture.componentInstance.setColorBand(ResistorColorBand.FirstBand);
+    let bandColor = fixture.debugElement.queryAll(By.css(theBandClassName))[ResistorColorBand.FirstBand];
     expect(bandColor.nativeElement.style.backgroundColor).toEqual(fixture.componentInstance.color);
   })
 
   it('the backgroundColor of the second band of the resistor shall be brown if select brown and the others have different color', () => {
     fixture.detectChanges();
     fixture.componentInstance.color = brownColor;
-    fixture.componentInstance.setColorBand(1);
-    let secondBandColor = fixture.debugElement.queryAll(By.css(theBandClassName))[1];
-    let firstBandColor = fixture.debugElement.queryAll(By.css(theBandClassName))[0];
-    let thirdBandColor = fixture.debugElement.queryAll(By.css(theBandClassName))[2];
-    let fourthBandColor = fixture.debugElement.queryAll(By.css(theBandClassName))[3];
+    fixture.componentInstance.setColorBand(ResistorColorBand.SecondBand);
+    let firstBandColor = fixture.debugElement.queryAll(By.css(theBandClassName))[ResistorColorBand.FirstBand];
+    let secondBandColor = fixture.debugElement.queryAll(By.css(theBandClassName))[ResistorColorBand.SecondBand];
+    let thirdBandColor = fixture.debugElement.queryAll(By.css(theBandClassName))[ResistorColorBand.ThirdBand];
+    let fourthBandColor = fixture.debugElement.queryAll(By.css(theBandClassName))[ResistorColorBand.FourthBand];
     expect(secondBandColor.nativeElement.style.backgroundColor).toEqual(fixture.componentInstance.color);
     expect(firstBandColor.nativeElement.style.backgroundColor).toEqual('');
     expect(thirdBandColor.nativeElement.style.backgroundColor).toEqual('');
@@ -42,16 +43,16 @@ describe('ResistorGenComponent (shallow tests)', () => {
   it('the backgroundColor of the third band of the resistor shall be yellow if select yellow ', () => {
     fixture.detectChanges();
     fixture.componentInstance.color = yellowColor;
-    fixture.componentInstance.setColorBand(2);
-    let bandColor = fixture.debugElement.queryAll(By.css(theBandClassName))[2];
+    fixture.componentInstance.setColorBand(ResistorColorBand.ThirdBand);
+    let bandColor = fixture.debugElement.queryAll(By.css(theBandClassName))[ResistorColorBand.ThirdBand];
     expect(bandColor.nativeElement.style.backgroundColor).toEqual(fixture.componentInstance.color);
   })
 
   it('the backgroundColor of the fourth band of the resistor shall be yellow if select yellow ', () => {
     fixture.detectChanges();
     fixture.componentInstance.color = yellowColor;
-    fixture.componentInstance.setColorBand(3);
-    let bandColor = fixture.debugElement.queryAll(By.css(theBandClassName))[3];
+    fixture.componentInstance.setColorBand(ResistorColorBand.FourthBand);
+    let bandColor = fixture.debugElement.queryAll(By.css(theBandClassName))[ResistorColorBand.FourthBand];
     expect(bandColor.nativeElement.style.backgroundColor).toEqual(fixture.componentInstance.color);
   })
 
